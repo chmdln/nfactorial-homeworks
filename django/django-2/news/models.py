66d6774b4c2631e26e1abfa226e38e48a12b84c3
tatyana <beatytanya@gmail.com>
2025-03-07 21:50:12 +0500

    Django part 2

---
+++ django/django-2/news/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now) 
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name="comments") 
+    
+    def __str__(self):
+        return self.content
+
+
+
